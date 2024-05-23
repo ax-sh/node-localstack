ca10cf58c066571d3e46e4d37d68b8a96e3ab8c8
Axmin <56112475+ax-sh@users.noreply.github.com>
2024-05-23 13:12:59 +0545

    docs: Update code comments for LocalStack configuration options

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -11,6 +11,19 @@ const lambda = new LambdaClient({
   }
 });
 
+// Call a Lambda API using the LocalStack endpoint
+// lambda.send(new ListFunctionsCommand({}))
+//     .then((data) => console.log(data))
+//     .catch((error) => console.error(error));
+
+// By default, @aws-sdk/client-s3 will using virtual host addressing:
+// -> http://<bucket-name>.s3.localhost.localstack.cloud:4566/<key-name>
+// To allow those requests to be directed to LocalStack, you need to set a specific endpoint.
+// If this is not possible, you can set the special S3 configuration flag to use path
+// addressing instead:
+// -> http://s3.localhost.localstack.cloud:4566/<bucket-name>/<key-name>
+// You can read the S3 documentation to learn more about the different endpoints.
+
 export const s3 = new S3Client({
   region: 'us-east-1',
   forcePathStyle: true, // If you want to use virtual host addressing of buckets, you can remove `forcePathStyle: true`.
